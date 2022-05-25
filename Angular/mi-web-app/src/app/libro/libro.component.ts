077555d04c65dd1aa42d7b883899a4724a2c72d8
jgyusey97 <61711332+jgyusey97@users.noreply.github.com>
2022-05-25 18:00:37 -0500

    Mejoras

--- Angular/mi-web-app/src/app/libro/libro.component.ts
+++ Angular/mi-web-app/src/app/libro/libro.component.ts
@@ -1,15 +1,24 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
-
+import { LibrosService } from '../services/libros.services';
 @Component({
   selector: 'app-libro',
   templateUrl: 'libro.component.html',
   styleUrls: ['libro.component.css'],
 })
 export class LibroComponent {
-  @Input() tituloLibro: string | undefined;
+  @Input() tituloLibro: string ="";
   @Output() libroClicked = new EventEmitter(); //Si va a viajar a un componente superior tiene que ser output
+  
+
+  //Creamos el objeto ya instanciado
+constructor(private librosService:LibrosService){ //Cuando se pasa algo en un argumento lo almacena la clase principal
+
+}
+
+ //Con la inyeccion de dependencias podemos realizar la eliminacion por medio de la interfaz que estamos inyectando
 
   onClicked() {
-    this.libroClicked.emit(); //Con esto el padre capturara el evento del hijo
+
+  this.librosService.eliminarLibro(this.tituloLibro);
   }
 }
--- Angular/mi-web-app/src/app/services/libros.services.ts
+++ Angular/mi-web-app/src/app/services/libros.services.ts
@@ -17,6 +17,19 @@ export class LibrosService {
     this.librosSubject.next(null);
   }
 
+ //Metodo para eliminarLibro
+ eliminarLibro(libroNombre : string){ 
+  
+  this.libros=this.libros.filter(x=>x!==libroNombre);  //Esta es la validacion para poder eliminar los libros 
+  //Cada vez que elimino algo debo de agregarlo a un evento para que pueda ser escuchada la accion
+  
+  this.librosSubject.next(null);
+
+  
+
+ }
+
+
   //Retornamos la lista de los libros
   obtenerLibros() {
     return [...this.libros]; //El sprit operador te permite retornar no solo los elementos existentes , sino los que se estan agregando a el
