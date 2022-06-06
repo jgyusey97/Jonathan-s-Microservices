7cb482909ce72c231b8af6ba298000bb9a48763b
jgyusey97 <61711332+jgyusey97@users.noreply.github.com>
2022-06-06 17:56:16 -0500

    Mejora
    Mejora

--- Angular/mi-web-app/src/app/books/books.component.ts
+++ Angular/mi-web-app/src/app/books/books.component.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { BooksService } from './books.service';
 import { Books } from './books.model';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit , AfterViewInit{
   bookData: Books[] = []; //Tenemos un arreglo de Libros
 
   desplegarColumnas = ['titulo', 'descripcion', 'autor', 'precio'];
 
   dataSource = new MatTableDataSource<Books>(); //El source de los datos
+  
+  @ViewChild(MatSort)
+  ordenamiento: MatSort = new MatSort() ;
+  constructor(private booksService: BooksService) {
 
-  constructor(private booksService: BooksService) {}
+    this.ordenamiento =  new MatSort();
+  }
+  hacerFiltro(filtro: Event) {
 
+      this.dataSource.filter = (filtro.target as HTMLInputElement).value;
+  }
   ngOnInit(): void {
     // this.bookData= this.booksService.obtenerLibros ();
 
     this.dataSource.data = this.booksService.obtenerLibros(); //Utilizamos los servicios para poder obtener la informacion
   }
+
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.ordenamiento;    
+  }
+  
 }
